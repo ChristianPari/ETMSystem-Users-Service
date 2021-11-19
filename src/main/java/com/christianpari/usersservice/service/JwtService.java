ed504288ae93525782677d768c0041154af1aa4c
Christian Pari <christianpari@outlook.com>
2021-11-18 19:46:37 -0500

    Changed ID PK of User so made findByUsername() and replaced instances of findById()

--- src/main/java/com/christianpari/usersservice/service/JwtService.java
+++ src/main/java/com/christianpari/usersservice/service/JwtService.java
@@ -40,14 +40,14 @@ public class JwtService implements UserDetailsService {
 
     String newGeneratedToken = jwtUtil.generateToken(userDetails);
 
-    User user = userDAO.findById(username).get();
+    User user = userDAO.findByUsername(username);
 
     return new JwtResponse(user, newGeneratedToken);
   }
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    User user = userDAO.findById(username).get();
+    User user = userDAO.findByUsername(username);
 
     if (user != null) {
       return new org.springframework.security.core.userdetails.User(
